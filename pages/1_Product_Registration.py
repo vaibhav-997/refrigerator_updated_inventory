eb74dc2a010da6e6f254aa0e891b4aec48cbba3e
Inamdar Vaibhav <143013678+vaibhav-997@users.noreply.github.com>
2025-05-02 11:29:56 +0530

    Update 1_Product_Registration.py

--- pages/1_Product_Registration.py
+++ pages/1_Product_Registration.py
@@ -44,7 +44,7 @@ name = st.text_input("Product Name")
 lot_number = st.text_input("Lot Number")
 mfg_date = st.date_input("Manufacture Date", datetime.date.today())
 expiry_date = st.date_input("Expiry Date", datetime.date.today())
-category = st.selectbox("Category", ["Cold Bottle", "Ice Cream", "Dairy Products", "Medical Products"])
+category = st.selectbox("Category", ["Cold Bottle", "Ice Cream", "Dairy Products", "Medical Products","Masale","Snacks"])
 
 if st.button("Register Product"):
     if insert_product(name, lot_number, mfg_date, expiry_date, category):
